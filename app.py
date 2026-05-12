b91bf0c211f5ae5a81f7a31c6cb19f3c3c2da07b
christianchueh <100501994+christianchueh@users.noreply.github.com>
2026-05-12 21:06:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ try:
 
     genai.configure(api_key=api_key)
 
-    model = genai.GenerativeModel('gemini-3.1-flash-lite')
+    model = genai.GenerativeModel('gemini-3-flash')
 
 except Exception as e:
     st.error(f"金鑰設定錯誤: {e}")
